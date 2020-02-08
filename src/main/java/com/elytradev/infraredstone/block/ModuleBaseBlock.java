94c082ee79d2b23250646023d4fe86b0df1f693d
Meredith Espinosa <goclonefilms@gmail.com>
2020-02-08 15:52:38 -0800

    add voxel shape for modules

--- src/main/java/com/elytradev/infraredstone/block/ModuleBaseBlock.java
+++ src/main/java/com/elytradev/infraredstone/block/ModuleBaseBlock.java
@@ -4,17 +4,24 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Waterloggable;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 import javax.annotation.Nullable;
 
 public abstract class ModuleBaseBlock extends Block implements Waterloggable {
 	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+
+	public static final VoxelShape BASE_SHAPE = VoxelShapes.cuboid(0, 0, 0, 1, 3/16f, 1);
 
 	public ModuleBaseBlock(Settings settings) {
 		super(settings);
@@ -31,4 +38,8 @@ public abstract class ModuleBaseBlock extends Block implements Waterloggable {
 		return this.getDefaultState().with(FACING, ctx.getPlayerFacing());
 	}
 
+	@Override
+	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext ePos) {
+		return BASE_SHAPE;
+	}
 }
