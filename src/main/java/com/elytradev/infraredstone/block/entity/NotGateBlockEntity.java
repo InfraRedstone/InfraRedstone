92636d877e763f954c0c76b329ee75828723a378
Alexandria <ada@achu.xyz>
2020-03-06 15:29:54 -0800

    added NOT gate logic that crashes the game. Yay!

---
+++ src/main/java/com/elytradev/infraredstone/block/entity/NotGateBlockEntity.java
@@ -0,0 +1,57 @@
+package com.elytradev.infraredstone.block.entity;
+
+import com.elytradev.infraredstone.api.InRedLogic;
+import com.elytradev.infraredstone.block.AndGateBlock;
+import com.elytradev.infraredstone.block.InRedBlocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.math.Direction;
+
+public class NotGateBlockEntity extends ModuleBaseBlockEntity {
+    private int lastSignal = -1;
+    private int signal = 0;
+
+
+    public NotGateBlockEntity(BlockEntityType<?> type) {
+        super(InRedBlocks.NOT_GATE_BE);
+    }
+
+    @Override
+    public int getOutputSignal() {
+        return signal;
+    }
+
+    @Override
+    public void updateSignal() {
+        BlockState state = getCachedState();
+        Direction front = state.get(AndGateBlock.FACING);
+        Direction back = front.getOpposite();
+        int signalBack = InRedLogic.findIRValue(world, pos, back);
+
+        int value = signalBack;
+        // bitwise NOT
+        value = ~value;
+
+        signal = value;
+        if (signal != lastSignal) {
+            lastSignal = signal;
+            if (!world.isClient) sync();
+        }
+    }
+
+    @Override
+    public void fromTag(CompoundTag tag) {
+        super.fromTag(tag);
+        tag.putInt("Signal", signal);
+        tag.putInt("LastSignal", lastSignal);
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        super.toTag(tag);
+        signal = tag.getInt("Signal");
+        lastSignal = tag.getInt("LastSignal");
+        return tag;
+    }
+}
